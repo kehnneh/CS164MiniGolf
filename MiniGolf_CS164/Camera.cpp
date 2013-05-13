59f393ba21b8ee3a82fbb4345bbc1b10e04e6aa5
kehnneh <leflame@gmail.com>
2013-05-13 01:14:28 -0700

    Added camera

--- MiniGolf_CS164/Camera.cpp
+++ MiniGolf_CS164/Camera.cpp
@@ -1,9 +1,36 @@
 #include "Camera.h"
+#include "CommonUtils.h"
+
+#include <glm\gtc\matrix_transform.hpp>
 
 Camera::Camera()
 {
+	projection = new glm::mat4;
+	matrix = new glm::mat4;
+}
+
+void Camera::Init(glm::vec3 eye, glm::vec3 target, glm::vec3 up)
+{
+	*matrix = glm::lookAt(eye, target, up);
 }
 
 Camera::~Camera()
 {
+	SAFE_DELETE(projection);
+	SAFE_DELETE(matrix);
+}
+
+void Camera::CreateProjectionMatrix()
+{
+	*projection = glm::perspective(45.f, 640.f/480.f, 0.1f, 10000.f);
+}
+
+glm::mat4 *Camera::GetMatrix()
+{
+	return matrix;
+}
+
+glm::mat4 *Camera::GetProjectionMatrix()
+{
+	return projection;
 }
--- MiniGolf_CS164/Camera.h
+++ MiniGolf_CS164/Camera.h
@@ -7,8 +7,15 @@ class Camera
 private:
 	glm::mat4 *matrix, *projection;
 
+	void CreateProjectionMatrix();
+
 public:
 	Camera();
 	~Camera();
+
+	void Init(glm::vec3 eye, glm::vec3 target, glm::vec3 up);
+
+	glm::mat4* GetMatrix();
+	glm::mat4* GetProjectionMatrix();
 };
 
